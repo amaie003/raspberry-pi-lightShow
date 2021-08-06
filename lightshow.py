3e7515f9773d69c40bded75900e9b2300bf81766
Efan <amaie003@ucr.edu>
2021-08-05 20:03:20 -0700

    Update lightshow.py

--- lightshow.py
+++ lightshow.py
@@ -1,23 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#!/usr/bin/env python3
-# rpi_ws281x library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
- 
-import time
+ import time
 from rpi_ws281x import *
 import argparse
 import math
 
 # LED strip configuration:
-LED_COUNT      = 30      # Number of LED pixels.
+LED_COUNT      = 40      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -33,16 +20,10 @@ def solidLightWipe(strip,color,wait_ms=50):
         time.sleep(wait_ms/1000)
  
 
-def breath(strip,color,wait_ms=50):
+def breath(strip,color,wait_ms=50,speed):
     increment = 1
     value = 0
-
-
- 
-    
-    
-
-    initialColor = List(color)
+    initialColor = list(color)
     print("start "+str(color[0])+","+str(color[1])+","+str(color[2]))
     for i in range(512):
         for i in range(strip.numPixels()):
@@ -52,13 +33,13 @@ def breath(strip,color,wait_ms=50):
         strip.show()
 
         if increment == 1:
-            color[0] = min(255, color[0]*1.1)
-            color[1] = min(255, color[1]*1.1)
-            color[2] = min(255, color[2]*1.1)
+            color[0] = min(255, color[0]*(1+speed))
+            color[1] = min(255, color[1]*(1+speed))
+            color[2] = min(255, color[2]*(1+speed))
         elif increment == -1:
-            color[0] = max(0, color[0]*0.9)
-            color[1] = max(0, color[1]*0.9)
-            color[2] = max(0, color[2]*0.9)
+            color[0] = max(0, color[0]*(1-speed))
+            color[1] = max(0, color[1]*(1-speed))
+            color[2] = max(0, color[2]*(1-speed))
         
         if (color[0] >= initialColor[0] or color[1] >= initialColor[1] or color[2] >= initialColor[2]):
             color[0] = initialColor[0]
@@ -88,19 +69,25 @@ if __name__ == '__main__':
         print('Use "-c" argument to clear LEDs on exit')
  
     try:
-     
-        while True:
-            breath(strip,[81,197,250],100)
- 
+        print("---------Welcome to the lightshow---------------")
+        print("Enter '0' for breathing mode")
+        print("Enter 'X' to exit")
+        print("------------------------------------------------")
+        option = input("Enter Option:")
+        if option == 0:
+            while True:
+                breath(strip,[81,197,250],70,0.05)
+        elif option == 'x' or option == 'X':
+            solidLightWipe(strip, Color(0,0,0), 10)
+            return
+
     except KeyboardInterrupt:
         if args.clear:
             solidLightWipe(strip, Color(0,0,0), 10)
 
 
-# In[ ]:Color(1,2,3)
 
 
 
 
 
-# %%
